506b3bdca90e6a6f6d9c049246fcf6fe8c176a99
Kaili Nishihira <kailinishihira@gmail.com>
2017-08-11 07:52:20 -1000

    spec 1 pass

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -3,13 +3,35 @@ using System;
 
 namespace WordCounter.Models
 {
-
   public class RepeatCounter
   {
+    private string _inputSentence;
+    private string _inputWord;
+    private int _wordCounter;
 
-    public RepeatCounter ()
+    public RepeatCounter (string inputSentence, string inputWord)
     {
+      _inputSentence = inputSentence;
+      _inputWord = inputWord;
     }
 
+    public int CountRepeats()
+    {
+      _wordCounter = 0;
+
+      if (_inputSentence.IndexOf(_inputWord) == -1)
+      {
+        _wordCounter = 0;
+      }
+      // string[] sentenceArray =_inputSentence.Split(' ');
+      // for (int i = 0; i < sentenceArray.Length; i++)
+      // {
+      //   if (sentenceArray[i].IndexOf(_inputWord) = -1)
+      //   {
+      //     _wordCounter = 0;
+      //   }
+      // }
+      return _wordCounter;
+    }
   }
 }
