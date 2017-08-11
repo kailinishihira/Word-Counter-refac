16c37af3b9bf8d2a4f9c3d1432e43a686cf0d951
Kaili Nishihira <kailinishihira@gmail.com>
2017-08-11 07:07:49 -1000

    fail spec 1 test

--- WordCounter.Tests/WordCounterTest.cs
+++ WordCounter.Tests/WordCounterTest.cs
@@ -8,18 +8,13 @@ namespace WordCounter.Tests
   [TestClass]
   public class RepeatCounterTest
   {
-    // [TestMethod]
-    // public void MethodToBeTested_BehaviorToBeTested_ExpectedResultOfTest()
-    // {
-    //   //Arrange
-    //   List<string> expected = new List<string> {"expectedOutput"};
-    //   ClassName newVariable = new ClassName("input");
-    //
-    //   //Act
-    //   List<string> actual = newVariable.MethodName();
-    //
-    //   //Assert
-    //   CollectionAssert.AreEqual(expected, actual);
-    // }
+    [TestMethod]
+    public void CountRepeats_DoesNotAppearInSentence_0()
+    {
+      RepeatCounter newCount = new RepeatCounter("hello", "hi");
+      int expected = 0;
+      int actual = newCount.CountRepeats();
+      Assert.AreEqual(expected, actual);
+    }
   }
 }
